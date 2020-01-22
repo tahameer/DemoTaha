9f4c0d26ba5cfcca8f3b80c61d2b331c07ad9942
Parent <Parent@DESKTOP-MGSJ2IO>
2020-01-22 09:17:25 -0800

    first commit

---
+++ CRUDPractice/DBContext/ProjectDBContext.cs
@@ -0,0 +1,20 @@
+﻿using CRUDPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CRUDPractice.DBContext
+{
+    public class ProjectDBContext :DbContext
+    {
+        public ProjectDBContext():base("ProjectDBContext")
+        {
+
+        }
+
+        public DbSet<EmployeeModel> Employee { get; set; }
+
+    }
+}
---
+++ CRUDPractice/Models/EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CRUDPractice.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+    }
+}
